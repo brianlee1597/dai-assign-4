bd4bbf351cf33213ac01551bca57a862ef49f072
Brian Lee <brianlee1597@gmail.com>
2023-03-21 03:20:29 -0400

    spacing

--- magnifier/scripts/magnifier.js
+++ magnifier/scripts/magnifier.js
@@ -12,11 +12,13 @@ jQuery(function () {
 
     $(document).on("keydown", function (e) {
         e.preventDefault();
+
         // if key is space
         if (e.key == " ") {
             toggleTextbox();
             return;
         }
+
         // else do graphic / non-graphic zoom
         const ZOOM = e.key == "-" ? -1 : e.key == "=" ? 1 : 0;
         const TSFM = e.key == "_" ? -1 : e.key == "+" ? 1 : 0;
@@ -32,10 +34,13 @@ jQuery(function () {
         const docWidth = $(document).width();
         const winWidth = $(window).width();
         const mouseX = e.pageX - $(this).scrollLeft();
+
         // guard clause: horizontal scroll not necessary
         if (docWidth <= winWidth) return;
+
         // assigns -, +, or null depending on mouse position
         const LR = mouseX < 100 ? '-' : mouseX > winWidth - 100 ? '+' : null;
+
         // depending on above variable, stop scrolling or scroll left / right
         !LR ? $('html, body').stop() 
         : $('html, body').animate({ scrollLeft: `${LR}=50` }, 50);
@@ -62,6 +67,7 @@ jQuery(function () {
             }, 
             text,
         }
+        
         // if textbox is true, remove textbox, else append
         !(textbox = !textbox) ? $("#magnifierDiv").remove()
         : $('body').append($("<div />", options));
