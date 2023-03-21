021b9ed53acd75bfc627376a86a1fd1c7f3c5f34
Brian Lee <brianlee1597@gmail.com>
2023-03-20 22:25:45 -0400

    push

--- magnifier/scripts/magnifier.js
+++ magnifier/scripts/magnifier.js
@@ -1,40 +1,30 @@
 jQuery(function () {
     let zoom = 1.0, scale = 1.0;
-    let mouseCoordX = null;
 
-    $(document).on("mousemove", function (event) {
-        mouseCoordX = event.pageX;
+    $(document).on("keydown", function (e) {
+        e.preventDefault();
+        const ZOOM = e.key == "-" ? -1 : e.key == "=" ? 1 : 0;
+        const TSFM = e.key == "_" ? -1 : e.key == "+" ? 1 : 0;
+
+        document.body.style.zoom = (zoom += ZOOM * 0.1);
+        $("body").css("transform",  `scale(${(scale += TSFM * 0.1)})`);
+
+        const DIFF = $(document).width() - $(window).width();
+
+        if (TSFM && DIFF > 0) {
+            $("body").css("position", "relative");
+            $("body").css("left", `${DIFF}px`);
+        }
     })
 
-    $(document).on("keydown", function (event) {
-        event.preventDefault();
-
-        if (event.shiftKey && event.key == "_") {
-            if ($(document).width() > $(window).width()) {
-                if (mouseCoordX <= 100) {
-                    
-                } else if ($(window).width() - mouseCoordX <= 100) {
-
-                }
+    $(document).on("mousemove", function (e) {
+        e.preventDefault();
+        if ($(document).width() > $(window).width()) {
+            if (e.pageX - $('html, body').scrollLeft() <= 100) {
+                $('html, body').animate({scrollLeft: `-=50`}, 100);
+            } else if ($(window).width() - e.pageX <= 100) {
+                $('html, body').animate({scrollLeft: `+=50`}, 100);
             }
-
-            $("body").css("transform",  `scale(${(scale -= 0.1)})`);
-        }
-
-        if (event.shiftKey && event.key == "+") {
-            if ($(document).width() > $(window).width()) {
-
-            }
-
-            $("body").css("transform",  `scale(${(scale += 0.1)})`);
-        }
-
-        if (event.key == "-") {
-            document.body.style.zoom = (zoom -= 0.1);
-        }
-
-        if (event.key == "=") {
-            document.body.style.zoom = (zoom += 0.1);
         }
     })
 })
