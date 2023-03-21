86ded9d024da12bb86a80db3ea5fd144048b5180
Brian Lee <brianlee1597@gmail.com>
2023-03-21 03:19:03 -0400

    comments

--- magnifier/scripts/magnifier.js
+++ magnifier/scripts/magnifier.js
@@ -1,7 +1,7 @@
 jQuery(function () {
-    let zoom = 1.0, scale = 1.0;
-    let textbox = false, text = '';
-
+    /**
+     * event listeners / extension logic
+    */
     $("*:not(body)").on("mouseenter", function (_) {
         text = $(this).text();
     })
@@ -10,36 +10,14 @@ jQuery(function () {
         text = '';
     })
 
-    function toggleMagnifier () {
-        if (!text && !textbox) return;
-
-        const options = {
-            id: "magnifierDiv",
-            css: {
-                "position": "fixed",
-                "left": "500px",
-                "top": "200px",
-                "font-size": "30px",
-                "z-index": "10",
-                "background": "white",
-            },
-            text
-        }
-
-        textbox ? $("#magnifierDiv").remove() 
-        : $('body').append($("<div />", options));
-
-        textbox = !textbox;
-    }
-
     $(document).on("keydown", function (e) {
         e.preventDefault();
-
+        // if key is space
         if (e.key == " ") {
-            toggleMagnifier();
+            toggleTextbox();
             return;
         }
-
+        // else do graphic / non-graphic zoom
         const ZOOM = e.key == "-" ? -1 : e.key == "=" ? 1 : 0;
         const TSFM = e.key == "_" ? -1 : e.key == "+" ? 1 : 0;
 
@@ -54,12 +32,38 @@ jQuery(function () {
         const docWidth = $(document).width();
         const winWidth = $(window).width();
         const mouseX = e.pageX - $(this).scrollLeft();
-        
-        if (docWidth > winWidth) {
-            const LR = mouseX < 100 ? '-' : mouseX > winWidth - 100 ? '+' : null;
-            
-            !LR ? $('html, body').stop() 
-            : $('html, body').animate({ scrollLeft: `${LR}=50` }, 50);
-        }
+        // guard clause: horizontal scroll not necessary
+        if (docWidth <= winWidth) return;
+        // assigns -, +, or null depending on mouse position
+        const LR = mouseX < 100 ? '-' : mouseX > winWidth - 100 ? '+' : null;
+        // depending on above variable, stop scrolling or scroll left / right
+        !LR ? $('html, body').stop() 
+        : $('html, body').animate({ scrollLeft: `${LR}=50` }, 50);
     });
+
+    /**
+     * extension states and extracted functions.
+    */
+    let zoom = 1.0, scale = 1.0;
+    let textbox = false, text = '';
+
+    function toggleTextbox () {
+        if (!text && !textbox) return;
+
+        const options = {
+            id: "magnifierDiv",
+            css: {
+                "position": "fixed",
+                "left": "500px",
+                "top": "200px",
+                "font-size": "30px",
+                "z-index": "10",
+                "background": "white",
+            }, 
+            text,
+        }
+        // if textbox is true, remove textbox, else append
+        !(textbox = !textbox) ? $("#magnifierDiv").remove()
+        : $('body').append($("<div />", options));
+    }
 })
