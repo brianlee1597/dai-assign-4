ab48cf99b8ae09d4008776843c734488e5b50d79
Brian Lee <brianlee1597@gmail.com>
2023-03-21 01:29:55 -0400

    pushed the non and graphic zooms

--- magnifier/scripts/magnifier.js
+++ magnifier/scripts/magnifier.js
@@ -6,25 +6,24 @@ jQuery(function () {
         const ZOOM = e.key == "-" ? -1 : e.key == "=" ? 1 : 0;
         const TSFM = e.key == "_" ? -1 : e.key == "+" ? 1 : 0;
 
-        document.body.style.zoom = (zoom += ZOOM * 0.1);
+        $("body").css("zoom", `${zoom += ZOOM * 0.1}`)
         $("body").css("transform",  `scale(${(scale += TSFM * 0.1)})`);
 
-        const DIFF = $(document).width() - $(window).width();
-
-        if (TSFM && DIFF > 0) {
+        if (TSFM) {
             $("body").css("position", "relative");
-            $("body").css("left", `${DIFF}px`);
+            $("body").css("transform-origin", "left top");
         }
     })
 
-    $(document).on("mousemove", function (e) {
-        e.preventDefault();
-        if ($(document).width() > $(window).width()) {
-            if (e.pageX - $('html, body').scrollLeft() <= 100) {
-                $('html, body').animate({scrollLeft: `-=50`}, 100);
-            } else if ($(window).width() - e.pageX <= 100) {
-                $('html, body').animate({scrollLeft: `+=50`}, 100);
-            }
-        }
-    })
+    // $(document).on("mousemove", function (e) {
+    //     e.preventDefault();
+    //     if ($(document).width() > $(window).width()) {
+    //         console.log(e.pageX);
+    //         // if (e.pageX - $('html, body').scrollLeft() <= 100) {
+    //         //     $('html, body').animate({scrollLeft: `-=50`}, 100);
+    //         // } else if ($(window).width() - e.pageX <= 100) {
+    //         //     $('html, body').animate({scrollLeft: `+=50`}, 100);
+    //         // }
+    //     }
+    // })
 })
